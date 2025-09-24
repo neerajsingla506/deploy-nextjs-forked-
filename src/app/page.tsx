5cc2aee72faa83dd763b784aa8a5f21aba2150d9
neerajsingla506 <neeraj.singla506@gmail.com>
2025-09-24 18:17:49 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and seevbnmnbvcxcvbnmnbvcx changes instantly.
+            Make changes instantly.
           </li>
         </ol>
 
